4642b7a7e8c66c77a8084b2ec7a1b6ce2b2c3a4f
Evan Gillogley <evanjmg@gmail.com>
2017-04-09 20:22:26 +0100

    move url clearing inside check

--- app/components/breadcrumb.ts
+++ app/components/breadcrumb.ts
@@ -39,8 +39,11 @@ export class BreadcrumbComponent implements OnInit, OnChanges {
         }
 
         this._routerSubscription = this.router.events.subscribe((navigationEnd:NavigationEnd) => {
-            this._urls.length = 0; //Fastest way to clear out array
-           if (navigationEnd instanceof NavigationEnd) this.generateBreadcrumbTrail(navigationEnd.urlAfterRedirects ? navigationEnd.urlAfterRedirects : navigationEnd.url);
+
+           if (navigationEnd instanceof NavigationEnd) {
+                this._urls.length = 0; //Fastest way to clear out array
+                this.generateBreadcrumbTrail(navigationEnd.urlAfterRedirects ? navigationEnd.urlAfterRedirects : navigationEnd.url);
+            }
         });
     }
 
